cd1ef073cb1a4e7b4aaac4952813fb27735163b9
AqsaKashaf <akashaf@andrew.cmu.edu>
2023-05-27 06:32:22 +0500

    box file list api added

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -15,12 +15,11 @@ Coded by www.creative-tim.com
 
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import App from "App";
 
 // Material Dashboard 2 React Context Provider
 import { MaterialUIControllerProvider } from "context";
-
 ReactDOM.render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
     <MaterialUIControllerProvider>
