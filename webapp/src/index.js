a09dd21debc846772dc41cf495905da2050b5fe1
AqsaKashaf <akashaf@andrew.cmu.edu>
2023-06-07 20:15:26 +0500

    https

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -21,7 +21,7 @@ import App from "App";
 // Material Dashboard 2 React Context Provider
 import { MaterialUIControllerProvider } from "context";
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <MaterialUIControllerProvider>
       <App />
     </MaterialUIControllerProvider>
