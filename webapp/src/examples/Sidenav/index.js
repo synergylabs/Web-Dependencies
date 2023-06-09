ebac89dd8c980478e1cafa2d05e435fb949caab4
AqsaKashaf <akashaf@andrew.cmu.edu>
2023-06-09 17:26:54 +0500

    logo

--- webapp/src/examples/Sidenav/index.js
+++ webapp/src/examples/Sidenav/index.js
@@ -165,7 +165,7 @@ function Sidenav({ color, brand, brandName, routes, ...rest }) {
           </MDTypography>
         </MDBox>
         <MDBox component={NavLink} to="/" display="flex" alignItems="center">
-          {brand && <MDBox component="img" src={brand} alt="Brand" width="2rem" />}
+          {brand && <MDBox component="img" src={brand} alt="Brand" width="1.5rem" />}
           <MDBox
             width={!brandName && "100%"}
             sx={(theme) => sidenavLogoLabel(theme, { miniSidenav })}
